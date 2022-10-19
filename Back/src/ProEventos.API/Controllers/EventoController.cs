dfdb82dafe27f8598f09737538043b5240419ddc
Renato Souza <tttrenato@hotmail.com>
2022-10-18 21:46:44 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                
+            };
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo post {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo delete {id}";
+        }
+    }
+}
