153dee31ae6219f3f4fd89412ef703ce1915cb33
Renato Souza <tttrenato@hotmail.com>
2022-10-20 22:01:20 -0300

    Seção 3: Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,18 +13,20 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-
+            _context =  context;
         }
 
         [HttpGet]
-        public Evento Get()
-        {
-            return new Evento(){
-                
-            };
+        public IEnumerable<Evento> Get(){
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id){
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
