fcee4ec804a618c8043293f585d055be1c13e106
Renato Souza <tttrenato@hotmail.com>
2022-12-13 22:27:57 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/Eventos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        public IEnumerable<Eventos> _evento = new Eventos[]
+        {
+            new Eventos(){
+                EventoId = 1,
+                Tema = "Angular 12 e .Net 5",
+                Local = "Rio Grande",
+                Lote = "1° Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+            }
+        };
+        public EventosController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Eventos> Get()
+        {
+            return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Eventos> GetById(int id)
+        {
+            return _evento.Where(eventos => eventos.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de post {id}";
+        }
+
+        [HttpPatch("{id}")]
+        public string Patch(int id)
+        {
+            return $"Exemplo de patch {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string HttpDelete(int id)
+        {
+            return $"Exemplo de delete {id}";
+        }
+    }
+}
--- Back/src/ProEventos.API/Models/Eventos.cs
+++ Back/src/ProEventos.API/Models/Eventos.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ProEventos.API.Models
 {
-    public class Evento
+    public class Eventos
     {
         public int EventoId { get; set; }
         public string Local { get; set; }
